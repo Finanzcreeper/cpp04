9adca85dbe553db8b28125bb9d65ecfb4d54b81e
Nico Reher <nreher@student.42berlin.de>
2023-09-06 15:33:31 +0200

    main updated

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,16 +6,16 @@
 int main()
 {
 const Animal* meta = new Animal();
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-const WrongAnimal* k = new WrongCat();
-std::cout << j->getType() << " " << std::endl;
-std::cout << i->getType() << " " << std::endl;
-std::cout << k->getType() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-k->makeSound();
 meta->makeSound();
-//...
+std::cout << "-------------------" << std::endl;
+const Animal* j = new Dog();
+j->makeSound();
+std::cout << "-------------------" << std::endl;
+const Animal* i = new Cat();
+i->makeSound();
+std::cout << "-------------------" << std::endl;
+const WrongAnimal* k = new WrongCat();
+k->makeSound();
+std::cout << "-------------------" << std::endl;
 return (0);
 }
